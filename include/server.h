deae4c0b80fd74ed15a77eedb23daeb2241b69ac
kath1031 <1203306166@qq.com>
2024-02-14 17:19:25 +0800

    随便加点

--- include/server.h
+++ include/server.h
@@ -61,47 +61,128 @@ namespace kath
                    fd2conn_[conn_fd] == nullptr);
             fd2conn_[conn_fd] = std::move(conn);
         }
-        
-        auto DelConn(std::shared_ptr<Conn> conn) ->void {
+
+        auto DelConn(Conn* conn) -> void
+        {
             fd2conn_.erase(conn->GetFd());
             conn->idle_node_.Detach();
         }
 
-        auto NextTimerMS() -> uint32_t {
+        auto AcceptConn() -> int32_t
+        {
+            struct sockaddr_in client_addr = {};
+            socklen_t socket_len = sizeof(client_addr);
+
+            int connfd =
+                accept(file_.Data(), reinterpret_cast<sockaddr *>(&client_addr), &socket_len);
+
+            if (connfd < 0)
+            {
+                Msg("accept() error");
+                return -1;
+            }
+            File conn_file{connfd};
+
+            std::shared_ptr<Conn> conn = std::make_shared<Conn>(std::move(conn_file), ConnState::STATE_REQ);
+            head_.InsertFront(&conn->idle_node_);
+
+            if (!conn)
+            {
+                return -1;
+            }
+            AddNewConn(conn);
+            return 0;
+        }
+        auto NextTimerMS() -> uint32_t
+        {
             uint64_t now_us = GetMonotonicUsec();
             uint64_t next_us = std::numeric_limits<uint64_t>::max();
 
-            if(!head_.Empty()) {
-                Conn *next = container_of(head_.next_,Conn,idle_node_);
-                next_us - std::min(next_us,next->idle_start_ +k_idle_timeout_ms * 1000 );
+            if (!head_.Empty())
+            {
+                Conn *next = container_of(head_.next_, Conn, idle_node_);
+                next_us - std::min(next_us, next->idle_start_ + k_idle_timeout_ms * 1000);
             }
             // todo 实现heap
             // if(!heap.Empty()) {
             //     next_us = std::min(next_us,heap.get_min());
             // }
-            if(next_us == std::numeric_limits<uint64_t>::max())
+            if (next_us == std::numeric_limits<uint64_t>::max())
                 return 10000;
-            if(next_us <= now_us)
+            if (next_us <= now_us)
                 return 0;
-            return static_cast<uint32_t>((next_us - now_us)/1000);
+            return static_cast<uint32_t>((next_us - now_us) / 1000);
         }
-        void join() {
+        void join()
+        {
             file_.SetNb();
             std::vector<struct pollfd> poll_args;
-            for(;;) {
+            for (;;)
+            {
                 poll_args.clear();
-                struct pollfd pfd = {file_.Data(),POLLIN,0};
+                struct pollfd pfd = {file_.Data(), POLLIN, 0};
                 poll_args.push_back(pfd);
 
-                for(const auto &[fd,conn] : fd2conn_) {
-                    if(!conn) continue;
+                for (const auto &[fd, conn] : fd2conn_)
+                {
+                    if (!conn)
+                        continue;
                     struct pollfd pfd = {};
                     pfd.fd = conn->GetFd();
                     pfd.events = conn->GetEvent() | POLLERR;
                     poll_args.push_back(pfd);
                 }
 
-                int timeout_ms = static_cast<int> (NextTimerMS());
+                int timeout_ms = static_cast<int>(NextTimerMS());
+                int rv =
+                    poll(poll_args.data(), static_cast<nfds_t>(poll_args.size()), timeout_ms);
+                if (rv < 0)
+                {
+                    Err("poll");
+                }
+
+                for (size_t index = 1; index < poll_args.size(); ++index)
+                {
+                    if (poll_args[index].revents)
+                    {
+                        auto conn = fd2conn_[poll_args[index].fd];
+
+                        conn->StartConnectionIO(&head_);
+                        if (fd2conn_[poll_args[index].fd]->IsEnd())
+                        {
+                            DelConn(conn.get());
+                        }
+                    }
+                }
+
+                ProcessTimers();
+                if (poll_args[0].revents)
+                {
+                    AcceptConn();
+                }
+            }
+        }
+        auto ProcessTimers() -> void
+        {
+            uint64_t now_us = GetMonotonicUsec();
+
+            while (!head_.Empty())
+            {
+                Conn *next = container_of(head_.next_, Conn, idle_node_);
+                uint64_t next_us = next->idle_start_ + k_idle_timeout_ms *1000;
+                if(next_us >= now_us +1000) {
+                    break;
+                }
+                DelConn(next);
+
+                // TTL timers
+                const size_t k_max_works = 2000;
+                size_t nworks = 0;
+
+                // while(!heap_.Empty() && heap_.GetMin() < now_us) {
+
+                // }
+
             }
         }
     };
